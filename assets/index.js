943b425bdcb6e2a39e8d32b623e74e9bccc77954
khoitv0302 <khoitv0302@gmail.com>
2021-08-03 21:38:09 +0700

    First commit template

---
+++ assets/index.js
@@ -0,0 +1,36 @@
+function showSubMenuGlass() {
+    var subMenu = document.getElementById("hideOrShowGlass");
+    var rotateDropDown = document.getElementById("rotateAngleDownGlass");
+    if (subMenu.style.display === "block") {
+      subMenu.style.display = "none";
+      rotateDropDown.style.transform = "rotate(0)";
+    } else {
+      subMenu.style.display = "block";
+      rotateDropDown.style.transform = "rotate(180deg)";
+    }
+
+  }
+  function showSubMenuScreen() {
+    var subMenu = document.getElementById("hideOrShowScreen");
+    var rotateDropDown = document.getElementById("rotateAngleDownScreen");
+    if (subMenu.style.display === "block") {
+      subMenu.style.display = "none";
+      rotateDropDown.style.transform = "rotate(0)";
+    } else {
+      subMenu.style.display = "block";
+      rotateDropDown.style.transform = "rotate(180deg)";
+    }
+  }
+  function showSubMenuAccessories() {
+    var subMenu = document.getElementById("hideOrShowAccessories");
+    var rotateDropDown = document.getElementById("rotateAngleDownAccessories");
+    if (subMenu.style.display === "block") {
+      subMenu.style.display = "none";
+      rotateDropDown.style.transform = "rotate(0)";
+    } else {
+      subMenu.style.display = "block";
+      rotateDropDown.style.transform = "rotate(180deg)";
+    }
+new Array
+  }
+
